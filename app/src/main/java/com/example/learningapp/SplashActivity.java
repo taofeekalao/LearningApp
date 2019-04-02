996adffb046d3c007a4eb45a5149192ae634b180
Tawfiq Alao <taofeekalao@gmail.com>
2019-04-02 12:26:15 +0100

    Initial commit of the project

---
+++ app/src/main/java/com/example/learningapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.learningapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
